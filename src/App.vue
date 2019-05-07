ae5ca9f229319aaf3b40155b78ccc19c354313d4
Twostupid <1379133507@qq.com>
2019-05-07 20:01:18 +0800

    TwoTtu-删除了hw中的ul

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <budilding></budilding>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'App'
+  import budilding from './components/B_dm_building_jimn'
+
+
+
+  export default {
+  name: 'App',
+    components:{
+      budilding,
+    }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
