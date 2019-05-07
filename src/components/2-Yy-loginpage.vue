b1ab881d37aec6292cb1ddcf93b7e5e82a879781
Yy199902 <1069093770@qq.com>
2019-05-07 19:28:52 +0800

    于印 登录组件

---
+++ src/components/2-Yy-loginpage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>$END$</div>
+</template>
+
+<script>
+    export default {
+        name: "2-Yy-loginpage",
+        data() {
+            return {
+                msg: ""
+            }
+        },
+        component: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
