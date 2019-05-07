1bece59d4a3eaa980a529369db097121cf6b67ef
Yy199902 <1069093770@qq.com>
2019-05-07 19:56:51 +0800

    于印 登录组件

---
+++ src/components/B_Yy_lgion.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+<!--      登录背景图-->
+      <div class="bgb">
+
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "T_Yy_lgion",
+        data() {
+            return {
+                msg: ""
+            }
+        },
+        component: {}
+    }
+</script>
+
+<style scoped>
+  .bgb{
+    width: 1200px;
+    height:650px;
+    background-image: url("http://yefengedu.com/static/img/bg_login.d936fa5.png");
+    background-repeat: no-repeat;
+
+  }
+
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="hello">
 
-
-
-
     <ul>
       <li>
         <a
