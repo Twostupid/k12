1a586fc010150184f8c176420d226f44d64a9904
Yy199902 <1069093770@qq.com>
2019-05-08 16:48:43 +0800

    Merge branch 'master' of https://github.com/Twostupid/k12
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/App.vue
    #	src/main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import $ from 'jquery'
-// import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 Vue.prototype.$axios = axios
 
