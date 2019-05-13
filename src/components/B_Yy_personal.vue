5799a464f4077b9bbfc7ffd7b013626a014ddbf4
Yy199902 <1069093770@qq.com>
2019-05-13 09:56:05 +0800

    跟人1

--- src/components/B_Yy_personal.vue
+++ src/components/B_Yy_personal.vue
@@ -1,95 +1,88 @@
 <template>
-    <div class="box">
-      <div class="personal">
-<!--      左面-->
+  <div class="box">
+    <div class="personal">
+      <!--      左面-->
 
-<div class="left">
-    <h3 class="left_one">个人中心</h3>
-<!--  图片-->
-  <div class="imgs">
-    <img :src="url" alt="">
-  </div>
-
-  <ul class="left_personal">
-     <h2>个人信息</h2>
-    <li>
-      <router-link to="/personal/datum">完善资料</router-link>
-    </li>
-    <li>
-      <router-link to="/personal/profile" >修改头像</router-link>
-    </li>
-    <li>
-      <router-link to="/personal/address">收货地址</router-link>
-    </li>
-    <li>
-      <router-link to="">我的订单</router-link>
-    </li>
-    <li>
-      <router-link to="">我的课程</router-link>
-    </li>
-    <li>
-      <router-link to="/personal/course">我的课程</router-link>
-    </li>
-
-    <li>
-      <router-link to="/personal/bps">我的积分</router-link>
-      <router-link to="">我的积分</router-link>
-    </li>
-    <li><router-link to="/personal/grow">我的成长值</router-link></li>
-    <li><router-link to="">关卡进度</router-link></li>
-    <li><router-link to="">勋章墙</router-link></li>
-    <li><router-link to="">课程兑换券</router-link></li>
-    <li><router-link to="">我的成长值</router-link></li>
-    <li><router-link to="/personal/progress">关卡进度</router-link></li>
-    <li><router-link to="/personal/medal">勋章墙</router-link></li>
-    <li><router-link to="/personal/course">课程兑换券</router-link></li>
-    <li class="juan">
-      <router-link to="">关卡兑换券</router-link></li>
-    <li><router-link to="">修改密码</router-link></li>
-    <h2>账号管理</h2>
-
-    <li><router-link to="">账户信息</router-link></li>
-    <li><router-link to="">账户充值</router-link></li>
-    <li><router-link to="">充值记录</router-link></li>
-    <li><router-link to="">消费计录</router-link></li>
-  </ul>
-</div>
-<!--      右面-->
-        <div class="right">
-          <router-view></router-view>
+      <div class="left">
+        <h3 class="left_one">个人中心</h3>
+        <!--  图片-->
+        <div class="imgs">
+          <img :src="url" alt="">
         </div>
 
+        <ul class="left_personal">
+          <h2>个人信息</h2>
+          <li>
+            <router-link to="/personal/datum">完善资料</router-link>
+          </li>
+          <li>
+            <router-link to="/personal/profile" >修改头像</router-link>
+          </li>
+          <li>
+            <router-link to="/personal/address">收货地址</router-link>
+          </li>
+          <li>
+            <router-link to="/personal/order">我的订单</router-link>
+          </li>
+
+          <li>
+            <router-link to="/personal/course">我的课程</router-link>
+          </li>
+
+          <li>
+            <router-link to="/personal/bps">我的积分</router-link>
+
+          </li>
+          <li><router-link to="/personal/grow">我的成长值</router-link></li>
+          <li><router-link to="/personal/progress">关卡进度</router-link></li>
+          <li><router-link to="/personal/medal">勋章墙</router-link></li>
+          <li><router-link to="/personal/courses">课程兑换券</router-link></li>
+          <li class="juan">
+            <router-link to="/personal/customs">关卡兑换券</router-link></li>
+          <li><router-link to="/personal/password">修改密码</router-link></li>
+          <h2>账号管理</h2>
+          <li><router-link to="">账户信息</router-link></li>
+          <li><router-link to="">账户充值</router-link></li>
+          <li><router-link to="">充值记录</router-link></li>
+          <li><router-link to="">消费计录</router-link></li>
+        </ul>
       </div>
+      <!--      右面-->
+      <div class="right">
+        <router-view></router-view>
+      </div>
+
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "",
-      data(){
-        return{
-          url:"",
+  export default {
+    name: "",
+    data(){
+      return{
+        url:"",
 
 
-        }
-      },
-      methods:{
-          num(){
-            this.url=this.$route.query.num
-          }
-
-      },
-      watch:{
-        $route(to,from){
-
-          this.url=this.$route.query.num
-        }
+      }
+    },
+    methods:{
+      num(){
+        this.url=this.$route.query.num
       }
 
+    },
+    watch:{
+      $route(to,from){
 
-
-
+        this.url=this.$route.query.num
+      }
     }
+
+
+
+
+  }
 </script>
 
 <style scoped>
@@ -98,7 +91,7 @@
   }
   .personal{
     width: 1200px;
-    height:830px;
+    height:860px;
     position: relative;
     left: 50%;
     margin-left: -600px;
@@ -108,7 +101,7 @@
   }
   .left{
     width: 190px;
-    height: 829px;
+    height: 859px;
     float: left;
     background-color: white;
   }
@@ -155,7 +148,7 @@
     height: 16px;
     /*margin-left: 40px;*/
     position: absolute;
-   color:rgb(0,0,0,0);
+    color:rgb(0,0,0,0);
     background-size: 100% 100%;
     background-image: url("http://yefengedu.com/static/images/v2.0/img/20190116164838.png");
   }
@@ -163,7 +156,7 @@
     margin: auto;
     width: 150px;
     height: 150px;
-   background-image: url("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1911150647,1930869014&fm=26&gp=0.jpg");
+    background-image: url("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1911150647,1930869014&fm=26&gp=0.jpg");
   }
   .imgs img{
     width: 150px;
